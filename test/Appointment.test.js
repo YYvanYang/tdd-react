292937e739db0d5ee03d8a835f89a246aa96c6ed
yvan.yvan <yvan@yvanyvandeMac-mini.local>
2020-05-30 13:05:13 +0800

    add appointment tests

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Appointment } from '../src/Appointment';
+import { Appointment, AppointmentsDayView } from '../src/Appointment';
 
 describe('Appointment', () => {
   let customer;
@@ -19,11 +19,37 @@ describe('Appointment', () => {
     expect(container.textContent).toMatch('Ashley');
   });
 
-  it('renders the customer first name', () => {
+  it('renders another customer first name', () => {
     customer = { firstName: 'Jordan' };
     container = document.createElement('div');
     render(<Appointment customer={customer} />);
 
     expect(container.textContent).toMatch('Jordan');
   });
+})
+
+describe('AppointmentsDayView', () => {
+  let container;
+
+  beforeEach(() => {
+    container = document.createElement('div');
+  })
+
+  const render = component => ReactDOM.render(component, container);
+
+  it('renders a div with the right id', () => {
+    render(<AppointmentsDayView appointments={[]} />);
+    expect(container.querySelector('div#appointmentsDayView')).not.toBeNull();
+  })
+
+  it('renders multiple appointments in an ol element', () => {
+    const today = new Date();
+    const appointments = [
+      { startsAt: today.setHours(12, 0) },
+      { startsAt: today.setHours(13, 0) }
+    ];
+    render(<AppointmentsDayView appointments={appointments} />);
+    expect(container.querySelector('ol')).not.toBeNull();
+    expect(container.querySelector('ol').children).toHaveLength(2);
+  })
 })
